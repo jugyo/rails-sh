0ab6bdc6ca99f79efd8387a04fa83dc2586dc557
jugyo <jugyo.org@gmail.com>
2011-01-24 15:27:22 +0900

    setup the readline each executed

--- lib/rails/sh.rb
+++ lib/rails/sh.rb
@@ -7,33 +7,39 @@ module Rails
     RAILS_SUB_COMMANDS = ['generate', 'destroy', 'plugin', 'benchmarker', 'profiler', 
                           'console', 'server', 'dbconsole', 'application', 'runner']
 
-    def self.start
-      Readline.basic_word_break_characters = ""
-      Readline.completion_proc = lambda do |word|
-        (Command.command_names + RAILS_SUB_COMMANDS).grep(/^#{Regexp.quote(word)}/)
+    class << self
+      def start
+        setup_readline
+        while buf = Readline.readline("\e[42mrails>\e[0m ", true)
+          execute(buf)
+          setup_readline
+        end
       end
 
-      while buf = Readline.readline("\e[42mrails>\e[0m ", true)
-        execute(buf)
+      def setup_readline
+        Readline.basic_word_break_characters = ""
+        Readline.completion_proc = lambda do |word|
+          (Command.command_names + RAILS_SUB_COMMANDS).grep(/^#{Regexp.quote(word)}/)
+        end
       end
-    end
 
-    def self.execute(line)
-      line = line.strip
-      if command = Command.find(line)
-        arg = line[/\s+\w+/].strip rescue nil
-        command.call(arg)
-      else
-        execute_rails_command(line)
+      def execute(line)
+        line = line.strip
+        if command = Command.find(line)
+          arg = line[/\s+\w+/].strip rescue nil
+          command.call(arg)
+        else
+          execute_rails_command(line)
+        end
       end
-    end
 
-    def self.execute_rails_command(line)
-      ENV.delete("RAILS_ENV")
-      ARGV.clear
-      ARGV.concat line.split(/\s+/)
-      puts "\e[42m$ rails #{ARGV.join(" ")}\e[0m"
-      load 'rails/commands.rb'
+      def execute_rails_command(line)
+        ENV.delete("RAILS_ENV")
+        ARGV.clear
+        ARGV.concat line.split(/\s+/)
+        puts "\e[42m$ rails #{ARGV.join(" ")}\e[0m"
+        load 'rails/commands.rb'
+      end
     end
   end
 end

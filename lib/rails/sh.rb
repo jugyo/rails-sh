a0cc8833a24c8ca9323e58a5c987a8b2c944a8e4
jugyo <jugyo.org@gmail.com>
2011-02-03 15:39:45 +0900

    reload before executing a rails command

--- lib/rails/sh.rb
+++ lib/rails/sh.rb
@@ -18,7 +18,6 @@ module Rails
         end
         after_fork do
           ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
-          ActionDispatch::Callbacks.new(Proc.new {}, false).call({})
         end
 
         init_rake
@@ -77,12 +76,17 @@ module Rails
         run_before_fork
         pid = fork do
           run_after_fork
+          reload!
           ARGV.clear
           ARGV.concat line.split(/\s+/)
           puts "\e[42m$ rails #{ARGV.join(" ")}\e[0m"
           require 'rails/commands'
         end
         Process.waitpid(pid)
+      end
+
+      def reload!
+        ActionDispatch::Callbacks.new(Proc.new {}, false).call({})
       end
     end
   end
